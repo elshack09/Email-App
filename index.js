ec23ee00523fc32ba9279dcbee173067e4b0cb61
elshack09 <elshack09@gmail.com>
2018-08-10 01:22:34 -0400

    testing access OAuth

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require("express");
-const passport = require("passport")
-const GoogleStrategy = require("passport-google-oauth20").Strategy
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 
-
- 
 app.get("/", (req, res) => {
   res.send({ msg: "Hi Erica" });
 });
 
-passport.use(new GoogleStrategy() )
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecrete: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accesToken)=>{
+      console.log(accesToken)
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
