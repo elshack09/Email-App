8dc4112dc42111026cedab49f2bbc052a29121de
elshack09 <elshack09@gmail.com>
2018-08-11 10:03:15 -0400

    oauth done

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done)=>{
+User.findById(id).then(user =>{
+    done(null,user)
+})
+})
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +23,19 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
-      console.log(refreshToken);
-      console.log(profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // user already exits
+          done(null, existingUser);
+        } else {
+          // create new user record
+          new User({
+            googleId: profile.id
+          })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
