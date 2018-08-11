a8612d8f60fc6cd84e9c5072b434fb4cf73c8f85
elshack09 <elshack09@gmail.com>
2018-08-11 10:08:15 -0400

    port update

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ app.get("/", (req, res) => {
   res.send({ msg: "Hi Erica" });
 });
 require("./routes/authRoutes")(app);
-const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+const portnum = process.env.portnum || 3000;
+app.listen(portnum);
