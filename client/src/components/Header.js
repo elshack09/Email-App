ee5181decff0ad3083117176d5079c2c20d62718
elshack09 <elshack09@gmail.com>
2018-08-30 13:02:56 -0400

    updates

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux'
 
 class Header extends Component {
   render() {
+    console.log(this.props)
     return (
       <nav>
         <div className="nav-wrapper pink accent-4" >
-          <a classname="left brand-logo">Email App</a>
+          <a className="left brand-logo">Email App</a>
           <ul className="right">
             <li>
               <a>Login With Google</a>
@@ -16,8 +18,10 @@ class Header extends Component {
     );
   }
 }
-
-export default Header;
+function mapStateToProps({auth}){
+  return{ auth}
+}
+export default connect(mapStateToProps) (Header);
 //  <nav>
 //           <div class="nav-wrapper">
 //             <a href="#" class="brand-logo">
@@ -25,7 +29,7 @@ export default Header;
 //             </a>
 //             <ul id="nav-mobile" class="right hide-on-med-and-down">
 //               <li>
-//                 <a href="sass.html">Sass</a>
+//                 <a href="sass.html">Sass</a>g
 //               </li>
 //               <li>
 //                 <a href="badges.html">Components</a>
