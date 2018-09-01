377dbbc1efd7534e367cd51cf057beff351b89e5
elshack09 <elshack09@gmail.com>
2018-09-01 00:21:21 -0400

    Landing component added

--- README.md
+++ README.md
@@ -22,6 +22,6 @@ Email App using Mongoose, Express, React, and Node with OAuth 2.0.
 # Deployment
 
 ## Heroku
-
+Link to application deployed on Heroku. 
 https://shielded-meadow-38665.herokuapp.com/
 
---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>
+                Email Application
+            </h1>
+            Feeback From Your Users
+        </div>
+    )
+}
+
+export default Landing
