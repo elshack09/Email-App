d6fbc3fb070e6ee6307dbc63473dbac7a3f3eac0
elshack09 <elshack09@gmail.com>
2018-09-22 11:54:34 -0400

    update

--- services/Mailer.js
+++ services/Mailer.js
@@ -11,6 +11,21 @@ class Mailer extends helper.Mail {
         this.body = new helper.Content('text/html', content)
         this.recipients = this.formatAddresses(recipients)
 
+        this.addContent(this.body)
+        this.addClickTracking()
+        this.addRecipients()
+    }
+    formatAddresses(recipients) {
+        return recipients.map(({ email }) => {
+            return new helper.Email(email)
+        })
+    }
+    addClickTracking() {
+        const trackingSettings = new helper.TrackingSettings()
+        const clickTracking = new helper.ClickTracking(true, true)
+
+        trackingSettings.setClickTracking(clickTracking)
+        this.addClickTrackingSettings(trackingSettings)
     }
 }
 
