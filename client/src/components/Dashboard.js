1539a438aaefdf14ac06e702cabcaa919cf9e986
elshack09 <elshack09@gmail.com>
2018-09-30 09:07:19 -0400

    added redux-form

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Dashboard = () => {
     return(
         <div>
         Dashboard
     <div className="fixed-action-btn">
-        <a className="btn-floating btn-large light-blue lighten-1">
+        <Link to="/surveys/new" className="btn-floating btn-large light-blue lighten-1">
         <i className="material-icons">add</i>
-        </a>
+        </Link>
     </div>
 
         </div>
